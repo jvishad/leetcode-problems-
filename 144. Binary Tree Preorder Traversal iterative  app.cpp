ce58628768f110ff1f7022830d3af8b3374bffc0
Vishad Jain <51223992+jvishad@users.noreply.github.com>
2021-06-05 15:54:33 +0530

    Update 144. Binary Tree Preorder Traversal iterative  app.cpp

--- 144. Binary Tree Preorder Traversal iterative  app.cpp
+++ 144. Binary Tree Preorder Traversal iterative  app.cpp
@@ -38,3 +38,43 @@ public:
         return ans;
     }
 };
+
+
+-------------------------------------------------------------------
+//     much better way
+    
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root == NULL){
+            return {};
+        }
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        st.push(root);
+        while(st.size()){
+            TreeNode* poping_node = st.top();
+            ans.push_back(poping_node->val);
+            st.pop();
+            if(poping_node->right!=NULL){
+                st.push(poping_node->right);
+            }
+            if(poping_node->left!=NULL){
+                st.push(poping_node->left);
+            }
+        }
+        return ans;
+    }
+};
